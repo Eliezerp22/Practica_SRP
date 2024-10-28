ee00a95a0ccf431cda67159a3acf919cd3fdf232
Eliezer Garcia <Eliezerp.garcia@hotmail.com>
2024-10-27 21:20:50 -0400

    Agregar archivos de proyecto.

---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Practica_SRP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    public class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string direccion;
+        private string correoElectronico;
+
+        public void ImprimirDatos()
+        {
+            Console.WriteLine($"Nombre: {nombre}");
+            Console.WriteLine($"Edad: {edad}");
+            Console.WriteLine($"Dirección: {direccion}");
+            Console.WriteLine($"Correo electrónico: {correoElectronico}");
+        }
+    }
+
+    public class ServicioCorreo
+    {
+        public void EnviarCorreoElectronico(string destinatario, string mensaje)
+        {
+            Console.WriteLine($"Enviando correo a {destinatario}: {mensaje}");
+        }
+    }
+}
